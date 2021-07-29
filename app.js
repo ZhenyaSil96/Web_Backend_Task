5918e8c7ed4d32955c3ba1c76727e561477665a6
Yevhenii <zhenya.sil96@gmail.com>
2021-07-29 13:47:39 +0300

    create server and connect MySQL

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'usersdb',
+    password: 'root',
+    port: '3306'
+})
+
+connection.connect( err => {
+    if(err){
+        return console.log('Error' + err.message)
+    }else {
+        console.log('Подключение к MySQL выполнено')
+    }
+})
+
+
+// app.get('/', (req, res) => {
+//     connection.query('SELECT * FROM users', (err, result) => {
+//         if(err) console.log(err)
+//         else res.send(result)
+//     })
+// })
+
+app.get('/', (req, res)=> {
+    res.send('<h1>Hi</h1>')
+})
+
+app.listen(3000, () => {
+    console.log('Server run')
+})
